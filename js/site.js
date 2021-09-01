1ef8307b4109b99da57b5e2bcb20da2f881e47a7
Courtney Bezuidenhout Rohrer <63608236+CourtneyRohrer@users.noreply.github.com>
2021-09-01 12:41:53 -0400

    first changes

--- js/site.js
+++ js/site.js
@@ -1,4 +1,27 @@
-//Call Hello World 
-function helloWorld(){
-    alert("Hello World");
+//get value from page inputString
+//starts or controller function
+
+function getString() {
+//get string from page
+
+//check for any capitals, spaces or punctuation marks - parse?
+
+//call palindrome
+
+//call displayData
 }
+
+
+//discover if the string is a palindrome
+//logic function
+
+function tacoCat() {
+    //reverse the string 
+
+    
+}
+
+
+
+//display the results with a message stating whether it is a palindrome or not 
+//view function
