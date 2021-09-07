7aa8a5a78bf943aba66101a7d185aef51e9bd2a7
Courtney Bezuidenhout Rohrer <63608236+CourtneyRohrer@users.noreply.github.com>
2021-09-07 11:58:25 -0400

    start of javascript

--- js/site.js
+++ js/site.js
@@ -1,27 +1,50 @@
 //get value from page inputString
 //starts or controller function
 
-function getString() {
+    function getString() {  
 //get string from page
+    let inputString = document.getElementById ("inputString").value;
 
-//check for any capitals, spaces or punctuation marks - parse?
+//remove all extra characters    
+     inputString = inputString.replace(/[^\w\s]|_/g, "")
+                                 .replace(/\s+/g, " ");
 
-//call palindrome
-
-//call displayData
+//call reverseString
+    let x = reverseString(inputString)
+//call tacoCat
+    tacoCat(x)
+        
 }
 
+//reverse the string 
+function reverseString(userString){
+
+    let revString = [];
+
+//Reverse a string using a for loop
+
+for (let index = userString.length - 1 ; index >= 0; index--) {
+
+    revString += userString[index];  
+}
+
+return revString;
+
+}
 
 //discover if the string is a palindrome
 //logic function
 
 function tacoCat() {
-    //reverse the string 
+    //check if inputString and reverseString are the same and display a message for both options
 
+    if (inputString === reverseString) {
+        document.write ("Congratulations, this is a palindrome!!!");
+    } else {
+        document.write ("Sorry, this is not a palindrome");
+    }
     
 }
-
-
 
 //display the results with a message stating whether it is a palindrome or not 
 //view function
