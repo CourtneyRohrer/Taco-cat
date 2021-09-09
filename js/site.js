133ab38e1c8d3da1c07b53b5cfcc7c406f14eda5
Courtney Bezuidenhout Rohrer <63608236+CourtneyRohrer@users.noreply.github.com>
2021-09-09 11:12:43 -0400

    completed the functionality

--- js/site.js
+++ js/site.js
@@ -1,50 +1,40 @@
-//get value from page inputString
-//starts or controller function
 
-    function getString() {  
-//get string from page
+function getString() {  
+
     let inputString = document.getElementById ("inputString").value;
 
-//remove all extra characters    
-     inputString = inputString.replace(/[^\w\s]|_/g, "")
-                                 .replace(/\s+/g, " ");
+            inputString = inputString
+                 .replace(/[^\w\s+]|_/g, "")
+                 .replace(/\s+/g, "").toLowerCase();
+    
+                   
 
-//call reverseString
     let x = reverseString(inputString)
-//call tacoCat
-    tacoCat(x)
-        
-}
 
-//reverse the string 
+  tacoCat(inputString, x) 
+}
+ 
 function reverseString(userString){
 
-    let revString = [];
+        let revString = [];
 
-//Reverse a string using a for loop
+  for (let index = userString.length - 1 ; index >= 0; index--) {
 
-for (let index = userString.length - 1 ; index >= 0; index--) {
-
-    revString += userString[index];  
+         revString += userString[index];  
 }
 
-return revString;
+
+    return revString;
 
 }
 
-//discover if the string is a palindrome
-//logic function
+function tacoCat(inputString, reverseString) {
 
-function tacoCat() {
-    //check if inputString and reverseString are the same and display a message for both options
-
-    if (inputString === reverseString) {
-        document.write ("Congratulations, this is a palindrome!!!");
+      if (inputString === reverseString) {
+        document.getElementById("msg").innerHTML = `Congratulations, this is a palindrome!!!`;
     } else {
-        document.write ("Sorry, this is not a palindrome");
+        document.getElementById("msg").innerHTML = `Sorry, this is not a palindrome`;
     }
-    
+        document.getElementById("alert").classList.remove("invisible");
+   
 }
-
-//display the results with a message stating whether it is a palindrome or not 
-//view function
